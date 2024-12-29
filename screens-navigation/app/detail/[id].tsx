2570675e43df1f359b139e2f712a08c32609e7e2
Anifyuli <anifyuliansyah@gmail.com>
2024-12-29 21:39:22 +0700

    upd: adding comments

--- screens-navigation/app/detail/[id].tsx
+++ screens-navigation/app/detail/[id].tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
-import DummyList from "@/data/dummyData.json"
+import DummyList from "@/data/dummyData.json" // Impor data JSON
 
+// Halaman detail yang menggunakan React Functional Components
 const DetailPage: React.FC = () => {
   const { id } = useLocalSearchParams();
   const item = DummyList.find((item) => item.id === Number(id));
 
+  // Pengkondisian jika item tidak ditemukan
   if (!item) {
     return (
       <View style={styles.container}>
@@ -15,6 +17,7 @@ const DetailPage: React.FC = () => {
     );
   }
 
+  // Munculkan tampilan jika item ditemukan
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{item.title}</Text>
@@ -23,6 +26,7 @@ const DetailPage: React.FC = () => {
   );
 };
 
+// Percantik tampilan
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16 },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 16 },
--- screens-navigation/app/index.tsx
+++ screens-navigation/app/index.tsx
@@ -7,15 +7,18 @@ import {
   StyleSheet,
 } from "react-native";
 import { useRouter } from "expo-router";
-import DummyList from "@/data/dummyData.json"
+import DummyList from "@/data/dummyData.json" // Impor data JSON
 
+// Halaman index dengan React Functional Component
 const IndexPage: React.FC = () => {
   const router = useRouter();
 
+  // Callback handleItemPress mengalihkan tampilan aplikasi ketika di klik salah satu anggota dari data yang diambil berdasarkan id
   const handleItemPress = (id: number) => {
     router.push(`/detail/${id}`);
   };
 
+  // Tampilkan hasilnya
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Dummy List</Text>
@@ -35,6 +38,7 @@ const IndexPage: React.FC = () => {
   );
 };
 
+// Percantik tampilan
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16 },
   header: { fontSize: 24, fontWeight: "bold", marginBottom: 16 },
